e48262a22d17328336ad93a91a5f49623303f842
piner <qjksxy@163.com>
2026-05-31 22:34:56 +0800

    fix-asset-paths-and-add-character-images

--- Code/Pools/ZZZCardPool.cs
+++ Code/Pools/ZZZCardPool.cs
@@ -8,12 +8,12 @@ public class ZZZCardPool : TypeListCardPoolModel
 {
     // 卡池的ID。必须唯一防撞车。
     public override string Title => "zzz_card_pool";
-    public override string EnergyColorName => "Black";
+    public override string EnergyColorName => "zzz_black";
 
     // 描述中使用的能量图标。大小为24x24。
-    public override string? TextEnergyIconPath => "res://ZZZMod/images/energy_test.png";
+    public override string? TextEnergyIconPath => "res://ZZZMod/images/character/energy.png";
     // tooltip和卡牌左上角的能量图标。大小为74x74。
-    public override string? BigEnergyIconPath => "res://ZZZMod/images/energy_test_big.png";
+    public override string? BigEnergyIconPath => "res://ZZZMod/images/character/energy_big.png";
 
     // 卡池的主题色。
     public override Color DeckEntryCardColor => new(0.5f, 0.5f, 1f);
--- Code/Pools/ZZZPotionPool.cs
+++ Code/Pools/ZZZPotionPool.cs
@@ -5,9 +5,9 @@ namespace ZZZMod.Code.Pools;
 public class ZZZPotionPool : TypeListPotionPoolModel
 {
     // 描述中使用的能量图标。大小为24x24。
-    public override string? TextEnergyIconPath => "res://ZZZMod/images/energy_test.png";
+    public override string? TextEnergyIconPath => "res://ZZZMod/images/character/energy.png";
     // tooltip和卡牌左上角的能量图标。大小为74x74。
-    public override string? BigEnergyIconPath => "res://ZZZMod/images/energy_test_big.png";
+    public override string? BigEnergyIconPath => "res://ZZZMod/images/character/energy_big.png";
 
-    public override string EnergyColorName => "Black";
+    public override string EnergyColorName => "zzz_black";
 }
